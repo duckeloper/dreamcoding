e489c7408e3a3fc8a3e8a175b976bb9bb5e32da7
Byungchan Park <duckeloper@gmail.com>
2022-10-01 17:48:23 +0900

    built-in chapter complete

---
+++ javascript/7.built-in/3.boolean.js
@@ -0,0 +1,23 @@
+// Boolean
+const isTrue = true; // new Boolean(true); 라고 표기해도 되지만 하지 않음
+console.log(isTrue.valueOf());
+
+/**
+ * Falsy
+ * 0
+ * -0
+ * null
+ * NaN
+ * undefined
+ * ''
+ */
+
+/**
+ * Truthy
+ * 1
+ * -1
+ * '0'
+ * 'false'
+ * []
+ * {}
+ */
---
+++ javascript/7.built-in/4.number.js
@@ -0,0 +1,48 @@
+const num1 = 123;
+const num2 = new Number(123); // 메모리 낭비
+console.log(typeof num1);
+console.log(typeof num2);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.NaN);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.POSITIVE_INFINITY);
+
+if (num1 < Number.MAX_VALUE) {
+  console.log('safe');
+}
+
+// 지수표기법
+const num3 = 102;
+console.log(num3.toExponential());
+
+// 반올림하여 문자열로 변환
+const num4 = 1234.52;
+console.log(num4.toFixed());
+
+console.log(num4.toString());
+console.log(num4.toLocaleString('ar-EG'));
+
+// 원하는 자릿수까지 유효하도록 반올림
+console.log(num4.toPrecision(5));
+console.log(num4.toPrecision(4));
+console.log(num4.toPrecision(2)); // 지수표기법
+
+console.log(Number.EPSILON); // 0과 1 사이에서 나타낼 수 있는 가장 작은 숫자
+if (Number.EPSILON > 0 && Number.EPSILON < 1) {
+  console.log(Number.EPSILON);
+}
+
+const num = 0.1 + 0.2 - 0.2; // 0.1?
+console.log(num);
+
+function isEqual(original, expected) {
+  return Math.abs(original - expected) <= Number.EPSILON;
+}
+
+console.log(isEqual(1, 1));
+console.log(isEqual(0.1, 0.1));
+console.log(isEqual(num, 0.1));
