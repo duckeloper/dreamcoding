3ff935c7d04a02c4b0c4cdbe41bec4ed6b4daa8a
Byungchan Park <duckeloper@gmail.com>
2022-10-04 10:33:57 +0900

    more-operators chapter complete

---
+++ javascript/11.more-operators/3.nullish.js
@@ -0,0 +1,9 @@
+// Nullish Coalescing Operator
+// ES11 (ECMAScript 2020)
+// ?? null, undefined 경우
+
+// || falsy한 경우 설정(할당)
+
+let num = 0;
+console.log(num || -1); // -1
+console.log(num ?? -1); // 0
