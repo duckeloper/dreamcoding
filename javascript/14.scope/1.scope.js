18ea73dd714f9c55e53daa43648a79016b8d7ab4
Byungchan Park <duckeloper@gmail.com>
2022-10-10 13:02:30 +0900

    scope chapter complete

---
+++ javascript/14.scope/1.scope.js
@@ -0,0 +1,24 @@
+// 코드 블럭: { }, if() { }, for() { }, function() { }
+// 블럭 외부에서는 블럭 내부의 변수를 참조할 수 없다
+
+{
+  const a = 'a';
+  console.log(a);
+}
+const b = 'b';
+// console.log(a); // ReferenceError: a is not defined
+
+// 함수 외부에서는 함수 내부의 변수를 참조할 수 없다
+function print() {
+  const message = 'Hello World!';
+  console.log(message);
+}
+
+// console.log(message); // ReferenceError
+
+// 함수 외부에서는 함수의 매개변수를 참조할 수 없다
+function sum(c, d) {
+  console.log(c, d);
+}
+
+// console.log(c, d); // ReferenceError
